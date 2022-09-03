7e3dcbc3a5fa9158ec743ecc25a913ff6e8c0bd5
hasanMahmud743 <h.mahmud.7430@gmail.com>
2022-09-03 09:23:26 +0600

    card added

--- newsFeed.js
+++ newsFeed.js
@@ -40,8 +40,53 @@ const cardFeture = (cardDetails) =>{
         
 
         const creat = document.createElement('div')
+        const details = cardDetail.details
+        const shortArr = details.split(' ')
+        const short = shortArr.slice(0, 40)
+        const join = short.join(' ')
+
+        console.log(join)
         creat.innerHTML = `
-        <img class="card-img m-5" src="${cardDetail.image_url}" alt="">
+        <div class="card rounded-3 my-5 p-4">
+             <div class="row">
+                <div class="col-md-5">
+                    <img class="card-img" src=" ${cardDetail.image_url}" alt="">
+                </div>
+
+                <div class="col-md-7">
+                        <h4 class="fw-bold"> ${cardDetail.title} </h4>
+                        <p> ${join} ....</p>
+
+
+                        <div class="d-flex pt-4 justify-content-between" >
+
+                            <div class="d-flex "> 
+                                <img class="author-img mx-3" src="${cardDetail.author.img}" alt="">
+
+                                <div class="lh-sm">
+                                    <p class="lh-1 fw-bold">${cardDetail.author.name}</p>
+                                    <p class="lh-1">${cardDetail.author.published_date}</p>
+                                </div>
+                            
+                            </div>
+
+                            <p> <span> <i class="fa-solid fa-eye"></i> </span>${cardDetail.total_view}</p>
+
+                            <p > <i class="fa-solid fa-star"></i>
+                                <i class="fa-solid fa-star"></i>
+                                <i class="fa-solid fa-star"></i> 
+                                <i class="fa-solid fa-star"></i>
+                                <i class="fa-solid fa-star-half"></i>
+                            </p>
+
+                            <p><i class="fa-solid text-danger fa-arrow-right"></i></p>
+                    </div>
+                </div>
+
+            </div>
+        
+
+         </div>
         `
 
         cardId.appendChild(creat)
